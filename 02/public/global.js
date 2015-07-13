b927a67221bb6b397447476147d71df30c9c89f5
Sam Stephen <samueljstephen@gmail.com>
2015-07-13 09:13:03 -0500

    02 Exercises/Questions Finished - routes handle request answers in home.erb

--- 02/public/global.js
+++ 02/public/global.js
@@ -1,7 +1,7 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/path1");
+req.open("get", "/path2");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
@@ -9,6 +9,11 @@ req.addEventListener("loadstart", function(){
 
 req.addEventListener("load", function(){
   // Your code for Exercise 1 goes here.
+  if (req.response == "yes") {
+    alert("Yay!")
+  } else {
+    alert("Awe, shucks!")
+  }
 });
 
 req.send();
