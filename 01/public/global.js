b07f9226dcca6512b1ea256bd2a851596dea9f5c
Sam Stephen <samueljstephen@gmail.com>
2015-07-12 19:08:41 -0500

    01 Exercise 2 - Changed path of request so alert shows no instead (info2)

--- 01/public/global.js
+++ 01/public/global.js
@@ -1,7 +1,7 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/info1");
+req.open("get", "/info2");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
